80352fc9d4c0f751ddf4f8d091d9e2987343acb6
helio-developer <helioendes.es@unitropico.edu.co>
2026-05-16 16:57:43 -0500

    Migrando PostgreSQL a Neon cloud

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const {Pool} = require("pg");
 
 const pool= new Pool({
-  user:"postgres",
-  host:"localhost",
-  database:"developmentfull",
-  password:process.env.DB_PASSWORD,
-  port: 5432
+  connectionString:process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 module.exports = pool;
